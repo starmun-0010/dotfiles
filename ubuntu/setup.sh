9ac422a9242d5e362405c5223139b959142312af
muqsit <abdemuqsit@hotmail.com>
2022-09-05 00:06:20 +0500

    Init

---
+++ ubuntu/setup.sh
@@ -0,0 +1,17 @@
+#Github
+sudo apt install git -y
+git config --global user.email abdemuqsit@hotmail.com
+git config --global user.name muqsit
+git config --global commit.gpgsign true
+
+#Tmux
+sudo apt install tmux -y
+ln -s "$PWD/.tmux.conf" ~/.tmux.conf
+tmux source-file ~/.tmux.conf
+
+#Fish
+sudo apt install fish -y
+
+#XClip
+sudo apt install xclip -y
+
