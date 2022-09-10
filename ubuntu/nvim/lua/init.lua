775e812f1238e17a82d59538254dccdaa3975153
muqsit <abdemuqsit@hotmail.com>
2022-09-10 19:00:16 +0500

    Added LSP keymappings

--- ubuntu/nvim/lua/init.lua
+++ ubuntu/nvim/lua/init.lua
@@ -52,6 +52,25 @@ local signature_config = {
 
 local lsp_on_attatch = function(_, bufnr)
     require"lsp_signature".on_attach(signature_config, bufnr) -- Note: add in lsp client on-attach
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = {noremap = true, silent = true, buffer = bufnr}
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder,
+                   bufopts)
+    vim.keymap.set('n', '<space>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
 end;
 
 local lsp_setup = {capabilities = capabilities, on_attach = lsp_on_attatch}
@@ -76,6 +95,15 @@ require("lspconfig").vimls.setup(lsp_setup)
 -- require("lspconfig").csharp_ls.setup(lsp_setup)
 require("lspconfig").sumneko_lua.setup(lsp_setup_lua)
 require("lspconfig").omnisharp.setup(lsp_setup_lua)
+
+-- Mappings
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = {noremap = true, silent = true}
+vim.keymap.set('n', '<C-.>', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '<C-p>', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', '<C-n>', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 -- Theme
 vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
