7583ecc44b3e06428985a386ca9af5a2d7ef65b4
muqsit <abdemuqsit@hotmail.com>
2022-09-13 23:28:25 +0500

    [NVim] Added cmp-nvim to lsp capabilities

--- ubuntu/nvim/lua/init.lua
+++ ubuntu/nvim/lua/init.lua
@@ -8,6 +8,7 @@ require("mason-lspconfig").setup({
 
 -- lsp-signature
 local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities.textDocument.completion.completionItem.resolveSupport = {
     properties = {"documentation", "detail", "additionalTextEdits"}
