549867fe0a8bce2d339255d369bd611f202fb468
muqsit <abdemuqsit@hotmail.com>
2022-09-24 20:22:44 +0500

    [NVim] Add vim commentary plugin

--- ubuntu/nvim/lua/plugins/plugins-init.lua
+++ ubuntu/nvim/lua/plugins/plugins-init.lua
@@ -5,6 +5,8 @@ local packer = require('packer')
 
 Module.setup = function()
     return packer.startup(function(use)
+        use 'wbthomason/packer.nvim'
+        use 'tpope/vim-commentary'
         packer_config.post_init(packer)
     end, packer_config.packer_config)
 end
