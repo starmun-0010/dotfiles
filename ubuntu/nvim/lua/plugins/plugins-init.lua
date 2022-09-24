d5f25404e180261466d653e525b28a59279a485b
muqsit <abdemuqsit@hotmail.com>
2022-09-24 20:32:13 +0500

    [NVim] Add NeoTree config

--- ubuntu/nvim/lua/plugins/plugins-init.lua
+++ ubuntu/nvim/lua/plugins/plugins-init.lua
@@ -9,9 +9,10 @@ Module.setup = function()
         use 'wbthomason/packer.nvim'
         use 'tpope/vim-commentary'
         use "fladson/vim-kitty"
-        
+
+        require('plugins/neotree-config').setup(use);
         require('plugins/telescope-config').setup(use);
-        
+
         packer_config.post_init(packer)
     end, packer_config.packer_config)
 end
