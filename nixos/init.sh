0519de53bccee73472aa19e3b1e95268b006c254
starmun-0010 <abdemuqsit@hotmail.com>
2024-05-02 13:09:19 +0500

    non nix kanata groups

---
+++ nixos/init.sh
@@ -0,0 +1,10 @@
+sudo groupadd uinput
+sudo groupadd input
+
+sudo usermod -aG input $USER
+sudo usermod -aG uinput $USER
+
+echo 'KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"' | sudo tee /etc/udev/rules.d/kanata
+
+sudo modprobe uinput
+
