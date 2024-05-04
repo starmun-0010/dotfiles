1a4832f151c4d08778fc783ee176f2a37f64e66d
starmun-0010 <abdemuqsit@hotmail.com>
2024-05-05 02:37:59 +0500

    2024-05-05 02:37 : id 3 -> /nix/store/vy9lks7vbdsjrb1p0bb6i1scwygbqani-home-manager-generation

--- nixos/home.sh
+++ nixos/home.sh
@@ -158,9 +158,13 @@ then
 	sudo ln -sfn "$PWD/programs/X11/xorg.conf" "/etc/X11/xorg.conf.d/20-intel.conf"
 fi
 
+if ! [ -L "/etc/localtime" ] || ! [ -e "/etc/localtime" ]; 
+then
+	sudo ln -sf /usr/share/zoneinfo/Asia/Karachi  /etc/localtime
+fi
+
 #github
 TOKEN=$(gh auth token 2>&1 >/dev/null)
-echo $TOKEN
 if [ "$TOKEN" == "no oauth token" ];
 then
 	rm ~/.config/git/config
