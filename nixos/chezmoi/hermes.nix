d1ef5afc3ce8a293bfc14f513fc14f8665bb73b3
starmun-0010 <abdemuqsit@hotmail.com>
2024-05-04 17:39:34 +0000

    2024-05-04 17:39 : id 5 -> /nix/store/ry7rs8g2gx4dv0nrk008p9i47x7avkan-home-manager-generation

--- nixos/chezmoi/hermes.nix
+++ nixos/chezmoi/hermes.nix
@@ -35,7 +35,7 @@
     text = (builtins.readFile ../programs/i3/config) + ''
       exec --no-startup-id nixGL picom -b 
       exec --no-startup-id "xinput set-prop 'SYNA801A:00 06CB:CEC6 Touchpad' 'libinput Tapping Enabled' 1"
- exec --no-startup-id 'xinput set-button-map 10 1 1 3 4 5 6 7'
+ exec --no-startup-id "xinput set-button-map 10 1 1 3 4 5 6 7"
       bindsym $mod+t exec nixGL wezterm
     '';
     force = true;
