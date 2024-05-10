d10905a86fdd01d1708faa0527d996897e7cebcb
starmun-0010 <abdemuqsit@hotmail.com>
2024-05-10 16:50:11 +0000

    changes

--- nixos/chezmoi/common.nix
+++ nixos/chezmoi/common.nix
@@ -8,6 +8,7 @@
   };
 
   home.packages = with pkgs; [
+    xdg-utils
     vifm
     i3blocks
     fira-code-nerdfont
--- nixos/chezmoi/hermes.nix
+++ nixos/chezmoi/hermes.nix
@@ -8,6 +8,8 @@
   home.username = "hermes";
   home.homeDirectory = "/home/hermes";
   home.packages = with pkgs; [
+    testdisk
+    ddrescue
     udiskie
     slack
     acpilight
@@ -32,6 +34,31 @@
   home.file.i3 = {
     enable = true;
     text = (builtins.readFile ../programs/i3/config) + ''
+      set $monitor_left "DP2"
+      set $monitor_right "HDMI-1"
+      set $monitor_internal "eDP1"
+      set $ws1 "1"
+      set $ws2 "2"
+      set $ws3 "3"
+      set $ws4 "4"
+      set $ws5 "5"
+      set $ws6 "6"
+      set $ws7 "7"
+      set $ws8 "8"
+      set $ws9 "9"
+      set $ws10 "10"
+ 
+ #assign workspace to monitor
+      workspace $ws1 output $monitor_left
+      workspace $ws2 output $monitor_right
+      workspace $ws3 output $monitor_internal
+      workspace $ws4 output $monitor_left
+      workspace $ws5 output $monitor_right
+      workspace $ws6 output $monitor_internal
+      workspace $ws7 output $monitor_left
+      workspace $ws8 output $monitor_right
+      workspace $ws9 output $monitor_internal
+      workspace $ws10 output $monitor_left
       exec --no-startup-id xrandr --output DP2  --preferred --left-of HDMI1 --output HDMI1  --preferred --left-of eDP1 --output eDP1 --preferred
       exec --no-startup-id nixGL picom -b
       exec --no-startup-id "xinput set-prop 'SYNA801A:00 06CB:CEC6 Touchpad' 'libinput Tapping Enabled' 1"
